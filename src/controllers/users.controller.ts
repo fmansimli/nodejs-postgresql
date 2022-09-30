20766ce56cc0ea4e9dac9a74d76e6834cb60f6d7
Farid Mansimli <f.mensimli@asan.gov.az>
2022-09-30 13:52:14 +0400

    prepared for postgresql ..

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express";
-import { User, ObjectId, IUser } from "../models";
+import { User, IUser } from "../models";
 import { Parser } from "../utils";
 
 export const getAll: RequestHandler = async (req, res, next) => {
@@ -7,19 +7,9 @@ export const getAll: RequestHandler = async (req, res, next) => {
   const projection = Parser.project(fields as string);
 
   try {
-    const total = await User.exec().countDocuments(query);
-    const pagination = Parser.paginate(total, +limit, +page);
-
-    const users = await User.exec()
-      .find(query)
-      .project(projection)
-      .skip((+page - 1) * +limit)
-      .limit(+limit)
-      .toArray();
-
     res.status(200).json({
-      body: { users },
-      pagination,
+      body: { users: [] },
+      pagination: {},
       meta: {
         url: req.originalUrl,
         ok: true,
@@ -33,11 +23,8 @@ export const getAll: RequestHandler = async (req, res, next) => {
 export const getById: RequestHandler = async (req, res, next) => {
   const projection = Parser.project(req.query.fields as string);
   try {
-    const _id = new ObjectId(req.params.id);
-    const user = await User.exec().findOne({ _id }, { projection });
-
     res.status(200).json({
-      body: { user },
+      body: { user: {} },
       meta: {
         url: req.originalUrl,
         ok: true,
@@ -53,14 +40,11 @@ export const create: RequestHandler = async (req, res, next) => {
   const user = new User(body);
 
   try {
-    const data = await User.exec().insertOne(user);
-    user._id = data.insertedId;
-
     res.status(201).json({
-      body: { user: data.acknowledged ? user : undefined },
+      body: { user: {} },
       meta: {
         url: req.originalUrl,
-        ok: data.acknowledged,
+        ok: true,
       },
     });
   } catch (error) {
@@ -72,16 +56,12 @@ export const updateById: RequestHandler = async (req, res, next) => {
   const user = req.body as IUser;
 
   try {
-    const _id = new ObjectId(req.params.id);
-    const data = await User.exec().updateOne({ _id }, { $set: user });
-    user._id = _id;
-
     res.status(200).json({
-      body: { user: data.modifiedCount ? user : undefined },
+      body: { user: {} },
       meta: {
         url: req.originalUrl,
-        ok: data.acknowledged,
-        effected: data.modifiedCount,
+        ok: true,
+        effected: 1,
       },
     });
   } catch (error) {
@@ -91,14 +71,11 @@ export const updateById: RequestHandler = async (req, res, next) => {
 
 export const deleteById: RequestHandler = async (req, res, next) => {
   try {
-    const _id = new ObjectId(req.params.id);
-    const data = await User.exec().deleteOne({ _id });
-
     res.status(200).json({
       meta: {
         url: req.originalUrl,
-        ok: data.acknowledged,
-        effected: data.deletedCount,
+        ok: true,
+        effected: 1,
       },
     });
   } catch (error) {
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,1 @@
-export { ObjectId } from "mongodb";
-
-export { Collection, ICollection } from "./collection";
-export { Area, IArea } from "./area";
-export { City, ICity } from "./city";
-export { IPlace, Place } from "./place";
-export { ITrip, Trip } from "./trip";
 export { IUser, User } from "./user";
-export { IEvent, Event } from "./event";
